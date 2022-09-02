31e233d1a4c7e37ddffe77b802abc6a871aaee6a
danield33 <danieldawit33@gmail.com>
2022-09-01 22:50:36 -0400

    Project init

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+
 
 export default function App() {
   return (
